4df8d0d50890cd192ebf86df8e73cc2cb30f09bd
UtsavDabhi16 <utsav@inexture.com>
2024-02-28 16:58:33 +0530

    first commit

---
+++ src/main/java/org/example/videocallappusingwebrtc/VideocallAppUsingwebRtcApplication.java
@@ -0,0 +1,13 @@
+package org.example.videocallappusingwebrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideocallAppUsingwebRtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideocallAppUsingwebRtcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/videocallappusingwebrtc/VideocallAppUsingwebRtcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.videocallappusingwebrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideocallAppUsingwebRtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
